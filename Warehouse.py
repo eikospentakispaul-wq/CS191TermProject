093bdeb3f7a0a8458d391000ccd1a2e23837052b
Georgios Lipas <georgelipas05@gmail.com>
2026-05-21 11:55:17 +0300

    Tim sort algorithm

--- Warehouse.py
+++ Warehouse.py
@@ -26,6 +26,12 @@ Storage=str(input("Do you wish to see the list of products and their prices in t
 
 
 if Storage=="yes":
+    Sorted = str(input("Would you like to see the product list sorted?").strip().casefold())
+    if Sorted=="yes":
+        sorted_products = sorted(Apple_products.items(), key=lambda x: x[1])
+    for product, price in sorted_products:
+        print(f'{product}: {price}€')
+if Sorted=="no":
     for product,price in products_list:
        print(f'{product}: {price}')
 if Storage=="no":
